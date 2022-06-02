47503dfbacdd5f9a66651ce84d58ceeb9e2160b8
jmoresca <jmoresca@outlook.com>
2022-06-02 10:51:58 +0200

    add lcov

--- client/jest.config.js
+++ client/jest.config.js
@@ -13,6 +13,11 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  // The directory where Jest should output its coverage files.
+  coverageDirectory: './coverage/',
+  // If the file path matches any of the patterns, coverage information will be skipped.
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
